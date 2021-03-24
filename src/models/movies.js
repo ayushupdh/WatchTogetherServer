709c9f67ce86c46843047bb514e4d40549cad7ba
ayushupdh <aayushupadhyay14@gmail.com>
2021-03-24 17:42:08 -0500

    MovieSchema now supports virtual refs

--- src/models/movies.js
+++ src/models/movies.js
@@ -2,89 +2,94 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const moviesSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  adult: {
-    type: Boolean,
-    required: true,
-  },
-  poster_path: {
-    type: String,
-    required: true,
-  },
-  genres: [
-    {
+const moviesSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    adult: {
+      type: Boolean,
+      required: true,
+    },
+    poster_path: {
+      type: String,
+      required: true,
+    },
+    genres: [
+      {
+        type: String,
+      },
+    ],
+    spoken_languages: [
+      {
+        type: String,
+      },
+    ],
+    overview: {
       type: String,
     },
-  ],
-  spoken_languages: [
-    {
+    production_companies: [
+      {
+        type: String,
+      },
+    ],
+    release_date: {
       type: String,
     },
-  ],
-  overview: {
-    type: String,
-  },
-  production_companies: [
-    {
-      type: String,
-    },
-  ],
-  release_date: {
-    type: String,
-  },
-  budget: {
-    type: Number,
-    required: true,
-  },
-  revenue: {
-    type: Number,
-    required: true,
-  },
-  trailer: {
-    site: {
-      type: String,
-    },
-    key: {
-      type: String,
-    },
-    size: {
+    budget: {
       type: Number,
+      required: true,
     },
-  },
-  runtime: {
-    type: Number,
-    required: true,
-  },
-  id: {
-    type: Number,
-    required: true,
-  },
-  imdb_id: {
-    type: String,
-  },
-  providers: [
-    {
-      display_priority: {
-        type: Number,
-      },
-      logo_path: {
+    revenue: {
+      type: Number,
+      required: true,
+    },
+    trailer: {
+      site: {
         type: String,
       },
-      provider_id: {
-        type: Number,
-      },
-      provider_name: {
+      key: {
         type: String,
       },
+      size: {
+        type: Number,
+      },
     },
-  ],
-});
+    runtime: {
+      type: Number,
+      required: true,
+    },
+    id: {
+      type: Number,
+      required: true,
+    },
+    imdb_id: {
+      type: String,
+    },
+    providers: [
+      {
+        display_priority: {
+          type: Number,
+        },
+        logo_path: {
+          type: String,
+        },
+        provider_id: {
+          type: Number,
+        },
+        provider_name: {
+          type: String,
+        },
+      },
+    ],
+  },
+  { toJson: { virtuals: true }, toObject: { virtuals: true }, id: false }
+);
 
+moviesSchema.set("toObject", { virtuals: true });
+moviesSchema.set("toJSON", { virtuals: true });
 const Movie = mongoose.model("Movie", moviesSchema);
 
 module.exports = Movie;
