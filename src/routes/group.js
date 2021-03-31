d02f07897e8c423b9102b429832ba428f43e6571
ayushupdh <aayushupadhyay14@gmail.com>
2021-03-31 17:03:18 -0500

    Created few routes for the new session modal

--- src/routes/group.js
+++ src/routes/group.js
@@ -10,6 +10,12 @@ const {
   getGroupUsers,
   deleteGroup,
   removeUserFromGroup,
+  createSession,
+  getActiveUsers,
+  addUserToSession,
+  removeUserFromSession,
+  getMoviesForSession,
+  addToMoviesLiked,
 } = require("../controllers/groupController");
 
 // Get all groups
@@ -17,6 +23,31 @@ router.get("/groups/getAll", auth, getAllGroups);
 
 // Delete all groups
 router.delete("/groups/dumpAll", auth, dumpAllGroups);
+
+// ----------------------------Session Routes---------------------------------------------
+
+// Create a new Session
+router.post("/groups/session", auth, createSession);
+
+// Get session info
+router.get("/groups/session", auth, () => {});
+
+// Get session info
+router.get("/groups/session/users", auth, getActiveUsers);
+
+// Add to active users
+router.post("/groups/session/addusers", auth, addUserToSession);
+
+//Remove from active users
+router.post("/groups/session/removeusers", auth, removeUserFromSession);
+
+//Get movies for this session
+router.get("/groups/session/movies", auth, getMoviesForSession);
+
+// Add to liked movies
+router.post("/groups/session/moviesliked", auth, addToMoviesLiked);
+
+// ----------------------------Session Routes Ends---------------------------------------------
 
 // Create a group
 router.post("/groups/create", auth, createGroup);
@@ -32,14 +63,6 @@ router.post("/groups/:id/users", auth, addUsertogroup);
 
 // Remove users from group
 router.delete("/groups/:groupId/users/:userId", auth, removeUserFromGroup);
-
-// Start a new session in group
-
-// Add to active users
-router.post("/groups/activeUsers", auth, () => {});
-
-// Get active users
-router.get("/groups/activeUsers", auth, () => {});
 
 // !Delete group  Needs to be updated
 router.delete("/groups/:id", auth, deleteGroup);
