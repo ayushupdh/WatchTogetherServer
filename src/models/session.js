3202f7459c541190749206fcfd4fae7e6c50dd87
ayushupdh <aayushupadhyay14@gmail.com>
2021-03-31 13:34:33 -0500

    Added session modal schema

---
+++ src/models/session.js
@@ -0,0 +1,78 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const sessionSchema = new Schema(
+  {
+    groupID: {
+      type: Schema.Types.ObjectId,
+      ref: "Group",
+      required: true,
+    },
+    active_users: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    movies_served: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Movie",
+      },
+    ],
+    movies_liked: [
+      {
+        movie: {
+          type: Schema.Types.ObjectId,
+          ref: "Movie",
+        },
+        liked_by: {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+        },
+        like_count: {
+          type: Number,
+        },
+      },
+    ],
+    params: {
+      genre: [
+        {
+          type: String,
+        },
+      ],
+      platform: [
+        {
+          type: String,
+        },
+      ],
+      lang: [
+        {
+          type: String,
+        },
+      ],
+    },
+    current_session_time: {
+      type: Number,
+      required: true,
+    },
+    admin: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    toJson: { virtuals: true },
+    toObject: { virtuals: true },
+    id: false,
+    timestamps: true,
+  }
+);
+
+sessionSchema.set("toObject", { virtuals: true });
+sessionSchema.set("toJSON", { virtuals: true });
+const Session = mongoose.model("Session", sessionSchema);
+
+module.exports = Session;
